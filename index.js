1876bd31f83db7b27c96ee5b354d74b2d715bd56
Hariprasanth <shhariprasanth@gmail.com>
2018-05-16 04:37:17 +0530

    Add title bar with app name

--- index.js
+++ index.js
@@ -1,4 +1,50 @@
-import { AppRegistry } from 'react-native';
-import App from './App';
+import React, { Component } from 'react';
+import {
+    AppRegistry,
+    Platform,
+    StyleSheet,
+    Text,
+    View
+} from 'react-native';
+
+import MainComponent from './app/components/MainComponent';
+
+
+class App extends Component<Props> {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <MainComponent/>
+                <Text style={styles.title}>
+                    Weather
+                </Text>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    title: {
+        position: 'absolute',
+        textAlign: 'center',
+        justifyContent: 'center',
+        lineHeight: 40,
+        alignContent: 'center',
+        backgroundColor: '#159cff',
+        color: '#ffffff',
+        width: '100%',
+        height: 40,
+        fontSize: 24,
+        fontWeight: 'bold'
+    },
+});
+
 
 AppRegistry.registerComponent('Weather', () => App);
